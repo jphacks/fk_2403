9b758126faf42dbbe26dd968bacdef9e700074f7
yoru5585 <yoru06460@gmail.com>
2024-10-26 18:07:06 +0900

    Scene4スクリプト書き中

--- fk_2403/Assets/kaisei2/Scripts/Scene4/CreateSendProfilePassword.cs
+++ fk_2403/Assets/kaisei2/Scripts/Scene4/CreateSendProfilePassword.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class CreateSendProfilePassword : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+    public void OnCreatePassword(string password){
         
     }
 }

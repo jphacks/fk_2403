cbb792cf12258d57e9fb504b5249bcb2aa8b84e0
Haruka Karakawa <rinnrenn424@yahoo.co.jp>
2024-10-27 05:03:50 +0900

    uidが端末に保存されるようにした

--- fk_2403/Assets/kaisei2/Scripts/All/UserDataManager.cs
+++ fk_2403/Assets/kaisei2/Scripts/All/UserDataManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UserDataManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class UserDataManager : MonoBehaviour
 
     private void Awake()
     {
+        //PlayerPrefs.DeleteAll();
         if (instance == null)
         {
             instance = this;
@@ -17,6 +19,28 @@ public class UserDataManager : MonoBehaviour
         else
         {
             Destroy(this.gameObject);
+        }
+
+        if (PlayerPrefs.HasKey("uid"))
+        {
+            UserDataManager.instance.uid = PlayerPrefs.GetString("uid", "");
+            if (uid != "")
+            {
+                SceneManager.LoadScene("Scene3");
+            }
+
+        }
+    }
+
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        //アプリがバックグラウンドに移行した時
+        if (pauseStatus)
+        {
+            Debug.Log("動いた");
+            PlayerPrefs.SetString("uid", UserDataManager.instance.uid);
+            PlayerPrefs.Save();
         }
     }
 }

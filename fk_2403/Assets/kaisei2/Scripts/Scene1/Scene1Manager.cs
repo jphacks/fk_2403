50ef9552411144381143e057c7e41b44d9a1f323
Haruka Karakawa <rinnrenn424@yahoo.co.jp>
2024-10-26 14:30:28 +0900

    シーン複製しまくった

--- fk_2403/Assets/kaisei2/Scripts/Scene1/Scene1Manager.cs
+++ fk_2403/Assets/kaisei2/Scripts/Scene1/Scene1Manager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI; // InputField用
 using Firebase;
 using Firebase.Auth;
+using Unity.VisualScripting;
 
 public class Scene1Manager : MonoBehaviour
 {
@@ -11,8 +12,22 @@ public class Scene1Manager : MonoBehaviour
     private Login login;
     private Register register;
 
+    //チュートリアルの画面遷移で使う値
+    private int tutorialnum = 0;
+
     public string email;
     public string password;
+
+
+
+    [SerializeField] GameObject SignInCanvas;
+    [SerializeField] GameObject TermsOfUseCanvas;
+    [SerializeField] GameObject LoginCanvas;
+    [SerializeField] GameObject TutorialCanvas;
+
+    [SerializeField]
+    GameObject[] TutorialPanel;
+
 
 
     // Start is called before the first frame update
@@ -39,4 +54,36 @@ public class Scene1Manager : MonoBehaviour
         // ログイン処理の実行
         login.SignIn(auth, email, password);
     }
+
+    public void onClicked_toSignIn()
+    {
+        //新規登録画面を表示
+        SignInCanvas.SetActive(true);
+    }
+    public void onClicked_toTermsOfUse()
+    {
+        //利用規約を表示
+        TermsOfUseCanvas.SetActive(true);
+    }
+
+    public void onClicked_toLogin()
+    {
+        //ログイン画面を表示
+        LoginCanvas.SetActive(true);
+    }
+
+    public void onClicked_toTutorial()
+    {
+        //チュートリアル画面を表示
+        TutorialCanvas.SetActive(true);
+    }
+
+    public void onClicked_nextTutorial()
+    {
+        TutorialPanel[tutorialnum].SetActive(true);
+        tutorialnum++;
+
+
+    }
+
 }

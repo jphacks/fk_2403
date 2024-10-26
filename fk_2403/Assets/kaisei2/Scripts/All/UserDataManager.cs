17fa989d465e39373bc41344017021ebf652c36c
yoru5585 <yoru06460@gmail.com>
2024-10-27 05:03:22 +0900

    a

--- fk_2403/Assets/kaisei2/Scripts/All/UserDataManager.cs
+++ fk_2403/Assets/kaisei2/Scripts/All/UserDataManager.cs
@@ -19,4 +19,24 @@ public class UserDataManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    public void SetUsernameToServer(string username){
+        FirebaseManager.instance.WriteData(uid+"/username", username);
+    }
+
+    public void SetAvatarDataToServer(string[] paths){
+        FirebaseManager.instance.WriteData(uid+"/eyes", paths[0]);
+        FirebaseManager.instance.WriteData(uid+"/mouth", paths[1]);
+        FirebaseManager.instance.WriteData(uid+"/eyebrow", paths[2]);
+        FirebaseManager.instance.WriteData(uid+"/hair", paths[3]);
+        FirebaseManager.instance.WriteData(uid+"/accessories", paths[4]);
+    }
+
+    public void SetRecieveProf(string profId){
+        FirebaseManager.instance.WriteData(uid+"/RecieveProf/"+profId, "");
+    }
+
+    public void SetResultProf(string profId){
+        FirebaseManager.instance.WriteData(uid+"/ResultProf/"+profId, "");
+    }
 }
